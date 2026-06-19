143661343e7d82fd1fb24189d2ff469dbec77608
Saereth <paulanthonydevs@gmail.com>
2026-06-19 06:05:09 -0700

    Fix Teleposer Focus recipe conflict

--- src/datagen/java/com/breakinblocks/neovitae/datagen/provider/NVRecipeProvider.java
+++ src/datagen/java/com/breakinblocks/neovitae/datagen/provider/NVRecipeProvider.java
@@ -796,9 +796,9 @@ public class NVRecipeProvider extends RecipeProvider {
                 .unlockedBy("has_altar", has(NVBlocks.ARA_VITAE.block().get()))
                 .save(output, NeoVitae.rl("bucket_life"));
 
-        // Teleposer Focus - ender pearl on tier 3 altar
+        // Teleposer Focus - eye of ender on tier 3 altar
         AltarRecipeBuilder.build(NVItems.TELEPOSER_FOCUS.get())
-                .from(Tags.Items.ENDER_PEARLS)
+                .from(Items.ENDER_EYE)
                 .minTier(3)
                 .bloodNeeded(2000)
                 .consumption(10)

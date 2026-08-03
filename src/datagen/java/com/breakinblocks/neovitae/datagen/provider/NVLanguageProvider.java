d5d0fbbb4ec01b2d5a37cc335e40058c71429093
Saereth <paulanthonydevs@gmail.com>
2026-08-03 08:07:38 -0700

    fix meteor display

--- src/datagen/java/com/breakinblocks/neovitae/datagen/provider/NVLanguageProvider.java
+++ src/datagen/java/com/breakinblocks/neovitae/datagen/provider/NVLanguageProvider.java
@@ -1086,6 +1086,7 @@ public class NVLanguageProvider extends LanguageProvider implements ModonomiconL
         addJei("recipe.meteor.fill", "Fill Block");
         addJei("recipe.meteor.weight", "Weight: %s");
         addJei("recipe.meteor.estimate", "Est: %s blocks (~%s%%)");
+        addJei("recipe.meteor.random_pool", "Random pick from %s blocks");
         addJei("recipe.arc", "Athanor");
         addJei("recipe.athanor.chance", "Chance: %s%%");
         addJei("recipe.athanor.spiritus_cost", "Spiritus Cost:");
